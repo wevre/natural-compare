75a18a2e925251d0ca50cb4ed26a859cea8197e4
wevre <6877948+wevre@users.noreply.github.com>
2024-06-22 10:58:23 -0600

    put back a lost test

--- test/wevre/natural_compare_test.clj
+++ test/wevre/natural_compare_test.clj
@@ -3,17 +3,22 @@
             [clojure.test :refer [deftest testing are is]]))
 
 (deftest natural-compare
-  (testing
+  (testing "sort"
    (are [coll out] (= out (vec (sort sut/natural-compare coll)))
      ["0" ""]                           ["" "0"]
      ["C0" "C"]                         ["C" "C0"]
      ["a1b2c3" "a1b2"]                  ["a1b2" "a1b2c3"]
      ["a1b2c3" "a1b2c"]                 ["a1b2c" "a1b2c3"]
+     ["a1b2c3" "a1b12c4" "a1b2"]        ["a1b2" "a1b2c3" "a1b12c4"]
      ["12" "2"]                         ["2" "12"]
      ["t3" "t1" "t10" "t12" "t2" "t27"] ["t1" "t2" "t3" "t10" "t12" "t27"]
      ["0010" "0001"]                    ["0001" "0010"]
      ["Märrklobb 20" "Märrklobb 1"]     ["Märrklobb 1" "Märrklobb 20"]
      ["10" "001"]                       ["001" "10"]))
+
+  (testing "overflow"
+    (is (= ["t1" "t2" "t3" "t10" "t12" "t27" "t11111111111111111111"]
+           (sort sut/natural-compare ["t3" "t1" "t11111111111111111111" "t10" "t12" "t2" "t27"]))))
 
   (testing "custom comparator"
     (is (= ["A10" "A9" "A5"]
